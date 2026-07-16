617d50a37315c3aad44c4d9d1c94e0905459b236
Alixoxox <Alixoxox@users.noreply.github.com>
2026-07-16 12:43:26 +0500

    fixed some bugs

--- config/redis.js
+++ config/redis.js
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const redisClient = createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+export const redisClient = createClient({ url: 'redis://redis:6379' });
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 await redisClient.connect();
 
